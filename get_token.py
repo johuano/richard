baf5ec5422c64d99d870bb22fae50b42c9663ea8
John Huang <johuano@gmail.com>
2014-06-11 16:32:11 +0800

    the first version

---
+++ get_token.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+import urlparse
+import oauth2 as oauth
+import sys,urllib,re
+from urllib2 import Request,urlopen
+
+consumer_key = 'be17b610736b5248511f9bab8244e814'
+consumer_secret = '04ac97fda668cf87860ae9fd182c58b5'
+
+request_token_url = 'http://fanfou.com/oauth/request_token'
+access_token_url = 'http://fanfou.com/oauth/access_token'
+authorize_url = 'http://fanfou.com/oauth/authorize'
+
+''' get the token '''
+consumer = oauth.Consumer(consumer_key, consumer_secret)
+client = oauth.Client(consumer)
+resp, content = client.request(request_token_url,"GET")
+if resp['status'] != '200':
+    raise Exception("Invalid response %s. " % resp['status'])
+request_token = dict(urlparse.parse_qsl(content))
+print "    - oauth_token        = %s" % request_token['oauth_token']
+print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
+
+''' get the oauth_token '''
+print "Go to the following link in your browser:"
+print "%s?oauth_token=%s&oauth_callback=oob" % (authorize_url, request_token['oauth_token'])
+
+accepted = 'n'
+while accepted.lower() == 'n':
+        accepted = raw_input('Have you authorized me? (y/n) ')
+oauth_verifier = raw_input('What is the PIN? ')
+
+token = oauth.Token(request_token['oauth_token'],
+                            request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+client = oauth.Client(consumer, token)
+
+resp, content = client.request(access_token_url, "GET")
+if resp['status'] != '200':
+        raise Exception("Invalid response %s." % resp['status'])
+access_token = dict(urlparse.parse_qsl(content))
+print "Access Token:"
+print "    - oauth_token        = %s" % access_token['oauth_token']
+print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
+print "You may now access protected resources using the access tokens above." 
+auth_token        = access_token['oauth_token']
+oauth_token_secret = access_token['oauth_token_secret']
+
+''' write into database '''
+import sqlite3
+conn = sqlite3.connect('oauth2.db')
+c = conn.cursor()
+c.execute('drop table token')
+c.execute('create table token(oauth_token varchar(100),oauth_token_secret varchar(100))')
+c.execute("insert into token(oauth_token,oauth_token_secret) values(?,?)",
+          [oauth_token,oauth_token_secret])
+conn.commit()
+conn.close()
+
+
---
+++ main.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+import os
+if os.path.exists('oauth2.db')== 'False':
+    import get_token.py
+
+import sqlite3
+conn = sqlite3.connect('oauth2.db')
+c = conn.cursor()
+c.execute('''select * from token''')
+oauth_token,oauth_token_secret = c.fetchone()
+conn.close()
+
+
+''' read the data '''
+import time
+import linecache
+no = int(time.strftime("%j"))
+message =  linecache.getline('data',no)
+
+
+''' send message to fanfou.com '''
+import urlparse,sys,urllib,re
+import oauth2 as oauth
+from urllib2 import Request,urlopen
+consumer_key = 'be17b610736b5248511f9bab8244e814'
+consumer_secret = '04ac97fda668cf87860ae9fd182c58b5'
+
+params={}
+params['status']=message
+url = 'http://api.fanfou.com/statuses/update.xml'
+consumer = oauth.Consumer(consumer_key, consumer_secret)
+token = oauth.Token(oauth_token,oauth_token_secret)
+request = oauth.Request.from_consumer_and_token(consumer,
+                                                token,                      
+                                                http_url=url,              
+                                                http_method='POST',
+                                                parameters=params    
+                                                )
+signature_method = oauth.SignatureMethod_HMAC_SHA1()
+request.sign_request(signature_method, consumer, token)
+def request_to_header(request, realm=''):
+    """Serialize as a header for an HTTPAuth request."""
+    auth_header = 'OAuth realm="%s"' % realm
+    for k, v in request.iteritems():
+        if k.startswith('oauth_') or k.startswith('x_auth_'):
+            auth_header += ', %s="%s"' % (k, oauth.escape(str(v)))
+    return {'Authorization': auth_header}
+
+headers=request_to_header(request)
+data = {'status':message}
+data = urllib.urlencode(data)
+resp = urlopen(Request(url,data=data,headers=headers))
+resp = resp.read()
+print resp
+
+
+''' well done, I do it :) '''
